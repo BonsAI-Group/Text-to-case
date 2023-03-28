27077c4e3c3c8171b903c496fbe48162d38834fd
Eli Brouwers <477782@student.fontys.nl>
2023-03-28 22:03:16 +0200

    Optimalization in answerQuestion so that it now only calls the model once

--- Prototype/Backend/zenya-form-prototype-backend/src/ml/QuestionAnswerModel.py
+++ Prototype/Backend/zenya-form-prototype-backend/src/ml/QuestionAnswerModel.py
@@ -6,4 +6,5 @@ class QuestionAnswerModel:
     
     def answerQuestion(self, question: str, context: str) -> tuple[str, float]:
         """Answer a question given a context. Returns the answer and the confidence."""
-        return self.model(question=question, context=context)["answer"], self.model(question=question, context=context)["score"]
+        result = self.model(question=question, context=context)
+        return result["answer"], result["score"]
