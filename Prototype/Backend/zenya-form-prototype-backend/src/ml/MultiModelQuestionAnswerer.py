a36cdca8d4fe84c765f412b96ba3f604056f310e
ericvdberge <i454038@fhict.nl>
2023-04-13 15:29:17 +0200

    applying multithreading + adding models to azure

--- Prototype/Backend/zenya-form-prototype-backend/src/ml/MultiModelQuestionAnswerer.py
+++ Prototype/Backend/zenya-form-prototype-backend/src/ml/MultiModelQuestionAnswerer.py
@@ -4,28 +4,38 @@ from .QuestionAnswerModel import QuestionAnswerModel
 
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
-
-
+from threading import Thread
 
 class MultiModelQuestionAnswerer(IQuestionAnswerer):
     def __init__(self):
         self.modelNames = {
-            "deberta": "deepset/deberta-v3-large-squad2",
-            # "distilbert": "distilbert-base-cased-distilled-squad",
-            "bert_large": "deepset/bert-large-uncased-whole-word-masking-squad2",
-            # "bert_base": "deepset/bert-base-cased-squad2",
-            "roberta": "deepset/roberta-base-squad2"
+            "deberta": "https://bert-large-uncased-who-1gsmt1ib.westeurope.inference.ml.azure.com/score",
+            # "deberta": "deepset/deberta-v3-large-squad2",
+            # # "distilbert": "distilbert-base-cased-distilled-squad",
+            "bert_large": "https://bert-large-uncased-who-1gsmt1ib.westeurope.inference.ml.azure.com/score",
+            # "bert_large": "deepset/bert-large-uncased-whole-word-masking-squad2",
+            # # "bert_base": "deepset/bert-base-cased-squad2",
+            # "roberta": "deepset/roberta-base-squad2",
+            "roberta": "https://bert-large-uncased-who-1gsmt1ib.westeurope.inference.ml.azure.com/score"
         }
         self.models = {}
-        for modelName in self.modelNames:
-            self.models[modelName] = QuestionAnswerModel(self.modelNames[modelName])
+        self.threads = [None] * len(self.modelNames)
+        
+        for i, modelName in enumerate(self.modelNames):
+            self.threads[i] = Thread(target=self.getQuestionAnswerResult, args=(modelName,))
+            self.threads[i].start()
+
+        for i, modelName in enumerate(self.modelNames):
+            self.threads[i].join()
 
         self.similarityModel = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
+    def getQuestionAnswerResult(self, modelName: str):
+        result = QuestionAnswerModel(self.modelNames[modelName])
+        self.models[modelName] = result
+
     def answerQuestion(self, question: str, context: str) -> Tuple[str, float]:
         """Answer a question given a context. Returns the answer and the confidence."""
-        
-
         answers = {}
         for modelName in self.models:
             answers[modelName] = self.models[modelName].answerQuestion(question, context)
--- Prototype/Backend/zenya-form-prototype-backend/src/ml/QuestionAnswerModel.py
+++ Prototype/Backend/zenya-form-prototype-backend/src/ml/QuestionAnswerModel.py
@@ -1,13 +1,30 @@
 from typing import Tuple
+import requests
+
+def query(model, payload):
+    response = requests.post(
+        model, 
+        headers={
+            'Authorization':'Bearer K64489zyHzCgPHmi3nFufOequK519ajO',
+        }, 
+        json=payload
+    )
+    return response.json()
 
 
 class QuestionAnswerModel:
     """Contains a single question answering model"""
     def __init__(self, model_path):
-        from transformers import pipeline
-        self.model = pipeline("question-answering", model=model_path, tokenizer=model_path)
+        # from transformers import pipeline
+        # self.model = pipeline("question-answering", model=model_path, tokenizer=model_path)
+        self.model = model_path
     
     def answerQuestion(self, question: str, context: str) -> Tuple[str, float]:
         """Answer a question given a context. Returns the answer and the confidence."""
-        result = self.model(question=question, context=context)
+        result = query(self.model, {
+            "inputs": {
+                "question": question,
+                "context": context
+            }
+        })
         return result["answer"], result["score"]
