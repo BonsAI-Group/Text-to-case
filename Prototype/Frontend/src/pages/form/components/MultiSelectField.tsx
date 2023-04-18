103a8f6ff6d8b6c05e72ea0813cbb3020b4f1e63
Eli Brouwers <477782@student.fontys.nl>
2023-04-18 22:02:48 +0200

    Add close button to multiselect

--- Prototype/Frontend/src/pages/form/components/MultiSelectField.tsx
+++ Prototype/Frontend/src/pages/form/components/MultiSelectField.tsx
@@ -1,27 +1,28 @@
-import { Group, MultiSelect, Text } from "@mantine/core";
+import { CloseButton, Group, MultiSelect, Text } from "@mantine/core";
 import { FieldAnswer, FormItem } from "../../../generated";
 import { useEffect, useState } from "react";
 import ConfidenceBubble from "./ConfidenceBubble";
 
 type MultiSelectFieldProps = {
   formItem: FormItem;
-  answer?: FieldAnswer;
+  answer?: FieldAnswer; 
   showConfidence?: boolean;
 };
 
 const MultiSelectField = ({formItem, answer, showConfidence} : MultiSelectFieldProps) : JSX.Element => {
-  const [value, setValue] = useState<string[]>(answer ? answer.answer : []);
+  const [selected, setSelected] = useState<string[]>([]);
 
   const findIndex = (value: string) => {
     return formItem.params!.findIndex((param) => param === value);
   };
 
   useEffect(() => {
-    setValue(answer ? answer.answer: []);
+    if (answer) {
+      setSelected(answer.answer);
+    }
   }, [answer]);
   return (
-    <MultiSelect data={formItem.params!} label={formItem.fieldName} value={value} onChange={setValue} valueComponent={(value) => (
-      // Gray box with padding and rounded corners, colored border if showConfidence
+    <MultiSelect data={formItem.params!} label={formItem.fieldName} value={selected} onChange={setSelected} valueComponent={(value) => (
       <div style={{backgroundColor: "#f0f0f0", 
                   padding: "2px 4px", 
                   borderRadius: "4px", 
@@ -29,11 +30,15 @@ const MultiSelectField = ({formItem, answer, showConfidence} : MultiSelectFieldP
                   marginTop: "4px",
                   marginBottom: "4px",
                   border: showConfidence ? `2px solid ${borderColor(answer ? answer.confidence[findIndex(value.label)] : 0)}` : "none"
-                  }}>
+                  }}
+                  >
         <Group spacing={"xs"}>
           <Text fz={"sm"}>{value.label}</Text>
           {answer && showConfidence ? <ConfidenceBubble confidence={answer ? answer.confidence[findIndex(value.label)] : 0} /> : null}
-          {/* <Text fz={"sm"}>{answer && showConfidence ? (answer.confidence[findIndex(value.label)]*100).toFixed(1) + "%" : null}</Text> */}
+          <CloseButton size="xs" onClick={() => {
+            setSelected(selected.filter((item) => item !== value.label));
+          }
+          } />
         </Group>
         
       </div>
