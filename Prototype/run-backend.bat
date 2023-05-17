5e46ada05539236706d6e4edd237540f499661e8
Eli Brouwers <44468876+Eliturbo@users.noreply.github.com>
2023-05-17 16:06:25 +0200

    Update run-backend.bat to suit SUTime library

--- Prototype/run-backend.bat
+++ Prototype/run-backend.bat
@@ -3,5 +3,6 @@ cd Backend\\zenya-form-prototype-backend
 python -m venv venv
 venv\\Scripts\\activate
 pip install -r requirements.txt
+mvn dependency:copy-dependencies -DoutputDirectory=./jars -f %cd%\venv\Lib\site-packages\sutime\pom.xml
 cd src
 python -m uvicorn main:app --reload
