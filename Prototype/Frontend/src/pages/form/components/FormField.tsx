6530611782d7fda0c715aaa357b406cbfdfe6d83
vcaf <victoria.fong.0408@gmail.com>
2023-05-19 21:51:31 +0200

    Fixed indentation

--- Prototype/Frontend/src/pages/form/components/FormField.tsx
+++ Prototype/Frontend/src/pages/form/components/FormField.tsx
@@ -26,9 +26,9 @@ const FormField = ({formItem, answer, showConfidence} : FormFieldProps) : JSX.El
     case FieldType.TEXT:
       field = <TextField formItem={formItem} answer={answer} showConfidence={showConfidence} />;
       break;
-      case FieldType.NUMERIC:
-        field = <NumericField formItem={formItem} answer={answer} showConfidence={showConfidence} />;
-        break;
+    case FieldType.NUMERIC:
+      field = <NumericField formItem={formItem} answer={answer} showConfidence={showConfidence} />;
+      break;
     case FieldType.RADIO_BUTTON:
       field = <RadioButtonsField formItem={formItem} answer={answer} showConfidence={showConfidence} />;
       break;
