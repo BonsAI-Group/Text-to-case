058c1e630f6e11db0c82c354bb4170a108a8da12
vcaf <victoria.fong.0408@gmail.com>
2023-05-19 21:50:03 +0200

    Is able to extract the first numerical digit.

--- Prototype/Backend/zenya-form-prototype-backend/src/ml/TextToNum.py
+++ Prototype/Backend/zenya-form-prototype-backend/src/ml/TextToNum.py
@@ -8,5 +8,13 @@ class TextToNum(ITextToNum):
 
     def textToNum(self, predicted_answer: str) -> str:
         numeric_answer = alpha2digit(predicted_answer, "en")
-        numeric_answer = re.sub("\D","",numeric_answer)
+        # Finding first number
+        numeric_answer = re.search(r'\d+', numeric_answer)  
+        if numeric_answer:
+            # Printing number and its index 
+            print('The first number is:', numeric_answer.group())
+            numeric_answer = numeric_answer.group()
+        else:
+            print('The given string does not have any number') 
+            numeric_answer= 0 
         return numeric_answer
