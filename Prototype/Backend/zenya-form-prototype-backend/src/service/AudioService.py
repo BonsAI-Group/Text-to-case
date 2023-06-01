7ba3796adf976c4aefa314cd89f47c5a5ab32e26
vcaf <victoria.fong.0408@gmail.com>
2023-06-01 23:56:55 +0200

    created audio service for endpoint to call

---
+++ Prototype/Backend/zenya-form-prototype-backend/src/service/AudioService.py
@@ -0,0 +1,21 @@
+from fastapi import UploadFile
+from ml.IQuestionGenerator import IQuestionGenerator
+from ml.InterrogativeQuestionGenerator import InterrogativeQuestionGenerator
+from ml.InterrogativeQuestionGenerator import InterrogativeQuestionGenerator
+from ml.SpeechToText import SpeechToText
+from ml.ISpeechToText import ISpeechToText
+
+class AudioService:
+    """Service for handling fields."""
+    def __init__(self):
+        self.QuestionGenerator: IQuestionGenerator = InterrogativeQuestionGenerator()
+        self.SpeechToText: ISpeechToText = SpeechToText()
+
+    def fillInAudioToText(self, spoken_context: bytes) -> str:
+        # path = os.getcwd() + '//audio_files//recording.wav'
+        # self.SpeechToText.speechToText(path)
+        self.SpeechToText.speechToText(spoken_context)
+        
+        """Fill in spoken context."""
+        """Checking the field Type"""
+        
