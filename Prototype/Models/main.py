86ffaee21d039b0577701f1f9f69e53d4c042da0
ericvdberge <i454038@fhict.nl>
2023-06-06 22:49:03 +0200

    implement model caching

--- Prototype/Models/main.py
+++ Prototype/Models/main.py
@@ -4,9 +4,15 @@ from RequestModels.question_answering_request import QuestionAnsweringRequest
 from RequestModels.multiple_choice_request import MultipleChoiceRequest
 app = FastAPI()
 
+models = {}
+
 @app.post("/question-answering")
 async def question_answering(request: QuestionAnsweringRequest):
-    model = pipeline("question-answering", model=request.model, tokenizer=request.model)
+    if request.model in models:
+        model = models[request.model]
+    else:
+        model = models[request.model] = pipeline("question-answering", model=request.model, tokenizer=request.model)
+
     result = model(request.payload.question, request.payload.context)
     return {
         "answer": result["answer"], 
@@ -15,7 +21,11 @@ async def question_answering(request: QuestionAnsweringRequest):
 
 @app.post("/multiple-choice")
 async def multiple_choice(request: MultipleChoiceRequest):
-    model = pipeline("zero-shot-classification", model=request.model)
+    if request.model in models:
+        model = models[request.model]
+    else:
+        model = models[request.model] = pipeline("zero-shot-classification", model=request.model)
+
     result = model(request.payload.context, candidate_labels=request.payload.candidate_labels, multi_label=True)
     return {
         "labels": result["labels"],
