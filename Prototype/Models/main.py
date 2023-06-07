e2a3004685d7aef5aa4e089a2976010971d64af8
ericvdberge <i454038@fhict.nl>
2023-06-07 12:18:19 +0200

    need to cache all models in to memory

--- Prototype/Models/main.py
+++ Prototype/Models/main.py
@@ -4,14 +4,22 @@ from RequestModels.question_answering_request import QuestionAnsweringRequest
 from RequestModels.multiple_choice_request import MultipleChoiceRequest
 app = FastAPI()
 
-models = {}
+models = {
+    "deepset/deberta-v3-large-squad2": pipeline("question-answering", model="deepset/deberta-v3-large-squad2", tokenizer="deepset/deberta-v3-large-squad2"),
+    "deepset/bert-large-uncased-whole-word-masking-squad2": pipeline("question-answering", model="deepset/bert-large-uncased-whole-word-masking-squad2", tokenizer="deepset/bert-large-uncased-whole-word-masking-squad2"),
+    "deepset/roberta-base-squad2": pipeline("question-answering", model="deepset/roberta-base-squad2", tokenizer="deepset/roberta-base-squad2"),
+    "microsoft/deberta-large-mnli": pipeline("zero-shot-classification", model="microsoft/deberta-large-mnli"),
+    "valhalla/distilbart-mnli-12-1": pipeline("zero-shot-classification", model="valhalla/distilbart-mnli-12-1")
+}
 
 @app.post("/question-answering")
 async def question_answering(request: QuestionAnsweringRequest):
     if request.model in models:
         model = models[request.model]
     else:
-        model = models[request.model] = pipeline("question-answering", model=request.model, tokenizer=request.model)
+        return {
+            "message": "Error: cannot find model"
+        }
 
     result = model(request.payload.question, request.payload.context)
     return {
@@ -24,7 +32,9 @@ async def multiple_choice(request: MultipleChoiceRequest):
     if request.model in models:
         model = models[request.model]
     else:
-        model = models[request.model] = pipeline("zero-shot-classification", model=request.model)
+        return {
+            "message": "Error: cannot find model"
+        }
 
     result = model(request.payload.context, candidate_labels=request.payload.candidate_labels, multi_label=True)
     return {
