462fc763a8dce8b06817df2cc54b3a7372b45083
ericvdberge <i454038@fhict.nl>
2023-06-27 14:33:29 +0200

    changed in codker and docker-compose

--- Prototype/Terraform/app-services.tf
+++ Prototype/Terraform/app-services.tf
@@ -1,46 +1,73 @@
-# Create the frontend webapp, pass in the App Service Plan ID
+
+
+# Create the frontend webapp, pass in the App Service Plan ID and backend URL
 resource "azurerm_linux_web_app" "frontend" {
-    name                                    = "webapp-${local.resource_prefix}-frontend"
-    location                                = azurerm_resource_group.rg.location
-    resource_group_name                     = azurerm_resource_group.rg.name
-    service_plan_id                         = azurerm_service_plan.appserviceplan.id
-    https_only                              = true
-    app_settings = {
-        DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"
-        DOCKER_REGISTRY_SERVER_USERNAME     = ""
-        DOCKER_REGISTRY_SERVER_PASSWORD     = ""
-        WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+  name                                    = "webapp-${local.resource_prefix}-frontend"
+  location                                = azurerm_resource_group.rg.location
+  resource_group_name                     = azurerm_resource_group.rg.name
+  service_plan_id                         = azurerm_service_plan.appserviceplan.id
+  https_only                              = true
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"
+    DOCKER_REGISTRY_SERVER_USERNAME     = ""
+    DOCKER_REGISTRY_SERVER_PASSWORD     = ""
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+    REACT_APP_API_URL                   = "https://webapp-${local.resource_prefix}-backend.azurewebsites.net"
+  }
+  site_config {
+    always_on                           = true
+    minimum_tls_version                 = "1.2"
+    application_stack {
+      docker_image     = local.frontend_docker_image
+      docker_image_tag = "latest"
     }
-    site_config {
-        always_on                           = false
-        minimum_tls_version                 = 1.2
-        application_stack {
-            docker_image     = local.frontend_docker_image
-            docker_image_tag = "latest"
-        }
-    }
+  }
 }
 
-# Create the backend webapp, pass in the App Service Plan ID
+# Create the backend webapp, pass in the App Service Plan ID and frontend URL
 resource "azurerm_linux_web_app" "backend" {
-    name                                    = "webapp-${local.resource_prefix}-backend"
-    location                                = azurerm_resource_group.rg.location
-    resource_group_name                     = azurerm_resource_group.rg.name
-    service_plan_id                         = azurerm_service_plan.appserviceplan.id
-    https_only                              = true
-    app_settings = {
-        DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"
-        DOCKER_REGISTRY_SERVER_USERNAME     = ""
-        DOCKER_REGISTRY_SERVER_PASSWORD     = ""
-        WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+  name                                    = "webapp-${local.resource_prefix}-backend"
+  location                                = azurerm_resource_group.rg.location
+  resource_group_name                     = azurerm_resource_group.rg.name
+  service_plan_id                         = azurerm_service_plan.appserviceplan.id
+  https_only                              = true
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"
+    DOCKER_REGISTRY_SERVER_USERNAME     = ""
+    DOCKER_REGISTRY_SERVER_PASSWORD     = ""
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+    FRONTEND_URL                        = "https://webapp-${local.resource_prefix}-frontend.azurewebsites.net"
+    MODEL_URL                           = "https://webapp-${local.resource_prefix}-models.azurewebsites.net"
+  }
+  site_config {
+    always_on                           = true
+    minimum_tls_version                 = "1.2"
+    application_stack {
+      docker_image     = local.backend_docker_image
+      docker_image_tag = "latest"
     }
-    site_config {
-        always_on                           = false
-        minimum_tls_version                 = 1.2
-        application_stack {
-            docker_image     = local.backend_docker_image
-            docker_image_tag = "latest"
-        }
-    }
+  }
 }
 
+# Create the models (hosting) webapp, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "models" {
+  name                                    = "webapp-${local.resource_prefix}-models"
+  location                                = azurerm_resource_group.rg.location
+  resource_group_name                     = azurerm_resource_group.rg.name
+  service_plan_id                         = azurerm_service_plan.appserviceplan.id
+  https_only                              = true
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"
+    DOCKER_REGISTRY_SERVER_USERNAME     = ""
+    DOCKER_REGISTRY_SERVER_PASSWORD     = ""
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+  }
+  site_config {
+    always_on                           = true
+    minimum_tls_version                 = "1.2"
+    application_stack {
+      docker_image     = local.models_docker_image
+      docker_image_tag = "latest"
+    }
+  }
+}
